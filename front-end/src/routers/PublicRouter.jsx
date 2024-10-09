96dd2547b14baf5e6ab9241e7c13ccfd1fc6595c
kzau1612 <ngohoangkim2002@gmail.com>
2024-10-09 10:58:02 +0700

    Phân trang

--- front-end/src/routers/PublicRouter.jsx
+++ front-end/src/routers/PublicRouter.jsx
@@ -1,13 +1,15 @@
 import Login from "../pages/Login/Login";
 import { Routes, Route, Navigate } from "react-router-dom";
+import ManageUser from "../pages/Manager/ManageUser/ManageUser";
+import UserList from "../pages/Manager/ManageUser/UserList";
 
 const publicRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/register" element={<Register />} />
-      <Route path="/verify" element={<Verify />} /> */}
+      <Route path="/manager/manage-user" element={<UserList />} />
+      {/* <Route path="/verify" element={<Verify />} /> */}
     </Routes>
   );
 };
