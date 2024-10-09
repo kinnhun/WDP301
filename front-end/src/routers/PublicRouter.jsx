8f4de9bd17220d0633f065e685cd9cd2975f4943
kzau1612 <ngohoangkim2002@gmail.com>
2024-10-09 11:14:31 +0700

    Add Layout

--- front-end/src/routers/PublicRouter.jsx
+++ front-end/src/routers/PublicRouter.jsx
@@ -2,16 +2,18 @@ import Login from "../pages/Login/Login";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ManageUser from "../pages/Manager/ManageUser/ManageUser";
 import UserList from "../pages/Manager/ManageUser/UserList";
+import Layout from "../layouts/Layout";
 
-const publicRouter = () => {
+const PublicRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/manager/manage-user" element={<UserList />} />
-      {/* <Route path="/verify" element={<Verify />} /> */}
+      <Route element={<Layout />}>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/manager/manage-user" element={<UserList />} />
+      </Route>
     </Routes>
   );
 };
 
-export default publicRouter;
+export default PublicRouter;
