4c79a7bb6004fc1425d378489fbd4559bbc94be4
kzau1612 <ngohoangkim2002@gmail.com>
2024-10-13 21:58:12 +0700

    commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,10 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Toaster } from "react-hot-toast";
-import "/public/css/light.css";
-import "/public/js/datatables.js";
-import "/public/js/fullcalendar.js";
-import "/public/js/settings.js";
+import "./assets/css/light.css";
+import "./assets/js/datatables.js";
+import "./assets/js/fullcalendar.js";
+import "./assets/js/settings.js";
 import PublicRouter from "./routers/PublicRouter";
 function App() {
   return (
