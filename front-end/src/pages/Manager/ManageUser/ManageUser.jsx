3234e9761bf1d8ac5aef5fd6386ae036a6deefaf
MinhAnh1801 <nguyenminhanh180102@gmail.com>
2024-10-21 07:47:50 +0700

    delete all

--- front-end/src/pages/Manager/ManageUser/ManageUser.jsx
+++
@@ -1,7 +0,0 @@
-import UserList from "./UserList";
-
-const ManageUser = () => {
-  return <UserList />;
-};
-
-export default ManageUser;
--- front-end/src/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-import { jwtDecode } from "jwt-decode";
-
-export const verifyAccessToken = (accessToken) => {
-  return jwtDecode(accessToken, import.meta.env.VITE_JWT_ACCESS_SECRET);
-};
-
-export const verifyCredential = (credential) => {
-  return jwtDecode(credential);
-};
