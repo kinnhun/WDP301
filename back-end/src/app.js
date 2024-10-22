3a1af50686e7379745fa8bbdd855090062580cd3
kzau1612 <ngohoangkim2002@gmail.com>
2024-10-23 00:35:19 +0700

    add create request

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 const authRouter = require("./routes/auth.route");
-const maintenanceRequests = require("./routes/MaintenanceRequests.route");
+const requestRouter = require("./routes/request.route");
 const profileRouter = require("./routes/profile");
 const Newrouter = require("./routes/new");
 app.use(cors());
@@ -13,9 +13,9 @@ app.use(express.json()); // Nhận body từ json
 app.use(express.urlencoded({ extended: true })); //Nhận body từ urlencoded
 
 //router
-app.use("/news",Newrouter)
+app.use("/news", Newrouter);
 app.use("/auth", authRouter);
-app.use("/api/maintenanceRequests", maintenanceRequests);
-app.use("/profile", profileRouter)
+app.use("/requests", requestRouter);
+app.use("/profile", profileRouter);
 
 module.exports = app;
---
+++ back-end/src/controllers/request.controller.js
@@ -0,0 +1,126 @@
+const RequestService = require("../services/request.service"); // Mô hình cho yêu cầu bảo trì
+const { successResponse, errorResponse } = require("../utils/response.js");
+
+// Lấy tất cả yêu cầu bảo trì
+const getAllRequests = async (req, res) => {
+  try {
+    const result = await RequestService.getAllRequests(); // Gọi đúng hàm getAllRequests từ model
+    const requests = result.recordset; // Truy xuất dữ liệu từ kết quả truy vấn SQL
+    return successResponse({
+      res,
+      message: "Lấy yêu cầu bảo trì thành công",
+      data: requests,
+    });
+  } catch (error) {
+    return errorResponse({
+      res,
+      status: 500,
+      message: "Lấy yêu cầu bảo trì thất bại",
+      errors: error.message,
+    });
+  }
+};
+
+// Tạo yêu cầu bảo trì mới
+const createRequest = async (req, res) => {
+  try {
+    const { room_id, user_id, description, request_type } = req.body;
+    // console.log(room_id, user_id, description, request_type);
+    if (!room_id || !user_id || !description || !request_type) {
+      return errorResponse({
+        res,
+        status: 400,
+        message: "Missing required fields",
+      });
+    }
+    const newRequest = await RequestService.createRequest(
+      room_id,
+      user_id,
+      request_type,
+      description
+    );
+
+    return successResponse({
+      res,
+      message: "Create request successfully",
+      data: newRequest,
+    });
+  } catch (error) {
+    return errorResponse({
+      res,
+      status: 500,
+      message: "Tạo yêu cầu bảo trì thất bại",
+      errors: error.message,
+    });
+  }
+};
+
+// Lấy yêu cầu bảo trì theo ID
+const getRequestById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const request = await RequestService.getRequestById(id);
+
+    return successResponse({
+      res,
+      message: "Lấy yêu cầu bảo trì thành công",
+      data: request,
+    });
+  } catch (error) {
+    return errorResponse({
+      res,
+      status: 500,
+      message: "Lấy yêu cầu bảo trì thất bại",
+      errors: error.message,
+    });
+  }
+};
+
+// Cập nhật yêu cầu bảo trì
+const updateRequest = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+    await MaintenanceRequest.updateRequest(id, updates);
+
+    return successResponse({
+      res,
+      message: "Cập nhật yêu cầu bảo trì thành công",
+    });
+  } catch (error) {
+    return errorResponse({
+      res,
+      status: 500,
+      message: "Cập nhật yêu cầu bảo trì thất bại",
+      errors: error.message,
+    });
+  }
+};
+
+// Xóa yêu cầu bảo trì
+const deleteRequest = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await MaintenanceRequest.deleteRequest(id);
+
+    return successResponse({
+      res,
+      message: "Xóa yêu cầu bảo trì thành công",
+    });
+  } catch (error) {
+    return errorResponse({
+      res,
+      status: 500,
+      message: "Xóa yêu cầu bảo trì thất bại",
+      errors: error.message,
+    });
+  }
+};
+
+module.exports = {
+  getAllRequests,
+  createRequest,
+  getRequestById,
+  updateRequest,
+  deleteRequest,
+};
